89c3658467ba776c1b088246d3ad027e28533133
Cloner ops <clonerops@gmail.com>
2023-11-12 15:53:57 +0330

    practice 35

--- practice35.js
+++ practice35.js
@@ -3,3 +3,19 @@
 // به ازای هر پاسخ درست یک امتیاز به کاربر بدهد 
 // مجموع امتیازات در کنسول نمایش داده شود
 
+const questions = [
+    {id: 1, question: "2 + 2", answer: "4"},
+    {id: 2, question: "3 + 2", answer: "5"},
+    {id: 3, question: "Javascript framework trend?", answer: "react"},
+    {id: 4, question: "10 * 2", answer: "20"},
+    {id: 5, question: "2 + 8", answer: "10"},
+]
+
+let score = 0
+questions.forEach((item) => {
+    let userAnswer = prompt(item.question)
+
+    if(userAnswer === item.answer) score ++
+})
+
+console.log(score)
