faaa3039c3c3c22a6fc3140198483b515b01b793
Cloner ops <clonerops@gmail.com>
2023-11-14 14:54:35 +0330

    practice41

---
+++ practice41/practice41.js
@@ -0,0 +1,6 @@
+// یک برنامه بنویسید که فرم هارا ولیدیشن کند بطوریکه نام کاربری 8 کاراکتر و پسورد 6 و هنگام تایپ کاربر چک کند
+// با ایونت بلور بنویسید
+
+let getUsernameInput = document.getElementById("typeUsernameX")
+let getPasswordInput = document.getElementById("typePasswordX")
+
